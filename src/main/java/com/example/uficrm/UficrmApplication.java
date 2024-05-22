a0395171693a7b66030313c9ee039957263cdfdc
Adela <adelaboduri24@gmail.com>
2024-05-23 00:44:39 +0200

    Initial Commit

---
+++ src/main/java/com/example/uficrm/UficrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.uficrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UficrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UficrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uficrm/UficrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uficrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UficrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
